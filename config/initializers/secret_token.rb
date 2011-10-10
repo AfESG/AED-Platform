d80f4a46b3a0fa79866092e608090379904d1079
Rob Heittman <heittman.rob@gmail.com>
2011-10-10 20:46:35 +0300

    Init commit - new skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aaed::Application.config.secret_token = 'cd9c2fffbfc135da5b8c8600574f37753810400d65e9ca9a78b13d61260d2712c02e947067ca2b8fbb43ec84d1eca2beca3d3e4c918d3e6b95ba8f82dbc10da2'
