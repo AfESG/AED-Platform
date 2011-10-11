395ffc6827946effbcee433d13e28dbaaa5324fa
Rob Heittman <heittman.rob@gmail.com>
2011-10-11 18:16:37 +0300

    Country pages consuming PostgreSQL queries

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -5,6 +5,37 @@ class ReportController < ApplicationController
     @continent = params[:continent]
     @region = params[:region].gsub('_',' ')
     @country = params[:country].gsub('_',' ')
+
+    @summary_totals_by_country = ActiveRecord::Base.connection.execute <<-SQL
+      SELECT *
+      FROM aed#{@year}.summary_totals_by_country where ccode='#{@country}'
+    SQL
+
+    @summary_sums_by_country = ActiveRecord::Base.connection.execute <<-SQL
+      SELECT *
+      FROM aed#{@year}.summary_sums_by_country where ccode='#{@country}'
+    SQL
+
+    @causes_of_change_by_country = ActiveRecord::Base.connection.execute <<-SQL
+      SELECT *
+      FROM aed#{@year}.causes_of_change_by_country where ccode='#{@country}'
+    SQL
+
+    @area_of_range_covered_by_country = ActiveRecord::Base.connection.execute <<-SQL
+      SELECT *
+      FROM aed#{@year}.area_of_range_covered_by_country where ccode='#{@country}'
+    SQL
+
+    @area_of_range_covered_sum_by_country = ActiveRecord::Base.connection.execute <<-SQL
+      SELECT *
+      FROM aed#{@year}.area_of_range_covered_sum_by_country where ccode='#{@country}'
+    SQL
+
+    @elephant_estimates_by_country = ActiveRecord::Base.connection.execute <<-SQL
+      SELECT *
+      FROM aed#{@year}.elephant_estimates_by_country where ccode='#{@country}'
+    SQL
+
   end
 
 end
