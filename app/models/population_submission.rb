17b4c6cf4daefa67fa517473058f50c2f283b713
Rob Heittman <heittman.rob@gmail.com>
2011-10-15 17:37:17 +0300

    Properly lay out routes. Fix pathspec bug.

--- app/models/population_submission.rb
+++ app/models/population_submission.rb
@@ -2,4 +2,26 @@ class PopulationSubmission < ActiveRecord::Base
 
   belongs_to :submission
 
+  has_many :survey_aerial_sample_counts
+  has_many :survey_aerial_sample_count_strata, :through => :survey_aerial_sample_counts
+
+  has_many :survey_aerial_total_counts
+  has_many :survey_aerial_total_count_strata, :through => :survey_aerial_total_counts
+
+  has_many :survey_dung_count_line_transects
+  has_many :survey_dung_count_line_transect_strata, :through => :survey_dung_count_line_transects
+
+  has_many :survey_faecal_dnas
+  has_many :survey_faecal_dna_strata, :through => :survey_faecal_dnas
+
+  has_many :survey_ground_sample_counts
+  has_many :survey_ground_sample_count_strata, :through => :survey_ground_sample_counts
+
+  has_many :survey_ground_total_counts
+  has_many :survey_ground_total_count_strata, :through => :survey_ground_total_counts
+
+  has_many :survey_individual_registrations
+
+  has_many :survey_others
+
 end
