f00f1029842add83406459b590956c6e012a04dc
Rob Heittman <heittman.rob@gmail.com>
2011-10-20 21:09:57 -0400

    More cosmetic and UX support

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,15 +1,2 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-
-function showRangeMaps(){
-  document.getElementById('rangemap').style.display='block';
-  document.getElementById('population').style.display='none';
-  document.getElementById('rangeMapLink').innerHTML="<span>Range Maps</span>";
-  document.getElementById('populationTableLink').innerHTML="<a href='javascript:showPopulationTables()'>Population Tables</a>";
-}
-
-function showPopulationTables(){
-  document.getElementById('rangemap').style.display='none';
-  document.getElementById('population').style.display='block';
-  document.getElementById('rangeMapLink').innerHTML="<a href='javascript:showRangeMaps()'>Range Maps</a>";
-}
