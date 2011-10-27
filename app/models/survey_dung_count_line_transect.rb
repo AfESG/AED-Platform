bb8c8264146b757de3575578e436270b087ca786
Alison Heittman <alison.heittman@gmail.com>
2011-10-27 12:49:25 -0400

    Validate survey at stratum level and data submitted on top levels of population survey types

--- app/models/survey_dung_count_line_transect.rb
+++ app/models/survey_dung_count_line_transect.rb
@@ -1,4 +1,6 @@
 class SurveyDungCountLineTransect < ActiveRecord::Base
+  validates_presence_of :surveyed_at_stratum_level
+  validates_presence_of :stratum_level_data_submitted
 
   belongs_to :population_submission
 
