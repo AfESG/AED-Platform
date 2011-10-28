b8b685cad6c416ebfc66696e8dbb29261e287540
Rob Heittman <heittman.rob@gmail.com>
2011-10-28 09:14:39 -0400

    Add plus signs in change interpreter tables
    Test #84

--- app/helpers/report_helper.rb
+++ app/helpers/report_helper.rb
@@ -1,2 +1,13 @@
 module ReportHelper
+
+  def signed_number(number)
+    if number.nil? or number == 0
+      '0'
+    elsif number.to_i>0
+      "+#{number_with_delimiter number}"
+    else
+      "#{number_with_delimiter number}"
+     end
+  end
+
 end
