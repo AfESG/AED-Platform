2206f399419436b6be860109146cf59eba7e2e01
Rob Heittman <heittman.rob@gmail.com>
2011-11-15 07:31:49 -0500

    Previous solution could nullcheck

--- app/helpers/report_helper.rb
+++ app/helpers/report_helper.rb
@@ -1,14 +1,18 @@
 module ReportHelper
 
   def signed_number(n)
-    number = n.to_i
-    if number.nil? or number == 0
+    if n.nil?
       '0'
-    elsif number.to_i>0
-      "+#{number_with_delimiter number}"
     else
-      "#{number_with_delimiter number}"
-     end
+      number = n.to_i
+      if number.nil? or number == 0
+        '0'
+      elsif number.to_i>0
+        "+#{number_with_delimiter number}"
+      else
+        "#{number_with_delimiter number}"
+      end
+    end
   end
 
 end
