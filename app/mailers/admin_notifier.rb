9ded465a8571f565dbcac7ab8b899f3cc10572ad
Rob Heittman <heittman.rob@gmail.com>
2011-12-01 14:05:57 -0500

    WIP view for admin message

--- app/mailers/admin_notifier.rb
+++ app/mailers/admin_notifier.rb
@@ -2,6 +2,7 @@ class AdminNotifier < ActionMailer::Base
   default :from => "network@elephantdatabase.org"
 
   def data_request_form_submitted(data_request_form)
+    @user = user
     @data_request_form = data_request_form
     mail :to => "Diane.Skinner@iucn.org", :cc => "heittman.rob@gmail.com"
   end
