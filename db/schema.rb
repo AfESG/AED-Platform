0297b4d3d8536414ca812888084446e56663d04c
Rob Heittman <heittman.rob@gmail.com>
2011-12-02 15:17:54 -0500

    Add Asian range states
    See #31

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111202033137) do
+ActiveRecord::Schema.define(:version => 20111202200027) do
 
   create_table "countries", :force => true do |t|
     t.string   "iso_code"
