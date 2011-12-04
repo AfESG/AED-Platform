4f4feec818e06492eb15ff71522628bc73c8e24f
Rob Heittman <heittman.rob@gmail.com>
2011-12-03 22:45:34 -0500

    Bugfixes and improvements of submission partial
    Should not crash the partial on missing (old) data from prior
    submissions. Better formatting of data entry mistakes.

--- app/controllers/population_submissions_controller.rb
+++ app/controllers/population_submissions_controller.rb
@@ -14,6 +14,7 @@ class PopulationSubmissionsController < ApplicationController
   # GET /population_submissions/1.xml
   def show
     @population_submission = PopulationSubmission.find(params[:id])
+    @submission = @population_submission.submission
 
     respond_to do |format|
       format.html # show.html.erb
