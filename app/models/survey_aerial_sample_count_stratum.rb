0f63ae0f1d3e1510056451cb7c6b0b2d29e4c3b2
Rob Heittman <heittman.rob@gmail.com>
2012-01-30 21:55:53 -0500

    WIP SurveyAerialSampleCountStratumValidator

--- app/models/survey_aerial_sample_count_stratum.rb
+++ app/models/survey_aerial_sample_count_stratum.rb
@@ -4,4 +4,27 @@ class SurveyAerialSampleCountStratum < ActiveRecord::Base
   validates_presence_of :population_estimate
 
   belongs_to :survey_aerial_sample_count
+
+  class SurveyAerialSampleCountStratumValidator < ActiveModel::Validator
+    def validate(record)
+      some_precision = false
+      [:population_variance,
+       :population_standard_error,
+       :population_t,
+       :population_degrees_of_freedom,
+       :population_confidence_limits,
+       :population_no_precision_estimate_available].each do |field|
+        value = record.send field
+        unless value.nil?
+          some_precision = true
+          break
+        end
+      end
+      unless some_precision
+        record.errors[:population_no_precision_estimate_available] << "must be checked if no other precision information is available"
+      end
+    end
+  end
+
+  validates_with SurveyAerialSampleCountStratumValidator
 end
