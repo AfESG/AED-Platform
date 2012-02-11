71b9240789e211a4891f37e4392a5d7ec7a1f241
Rob Heittman <heittman.rob@gmail.com>
2012-02-11 09:23:24 +0300

    Use S3 storage

--- app/models/population_submission_attachment.rb
+++ app/models/population_submission_attachment.rb
@@ -1,5 +1,13 @@
 class PopulationSubmissionAttachment < ActiveRecord::Base
 
   belongs_to :population_submission
+  has_attached_file :file,
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET'],
+      :access_key_id => ENV['S3_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['S3_SECRET_ACCESS_KEY']
+    }
+    :path => "/:id/:filename"
 
 end
