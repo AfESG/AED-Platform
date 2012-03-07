9fcd0d4042bd88b521cf11d36ac9d45f9822d8c9
Rob Heittman <heittman.rob@gmail.com>
2012-03-07 17:04:17 -0500

    "Current" reference database source exposed
    This creates the links to show the references exposed from the
    aed2007 / aedcurrent references table. To produce the AESRs, these
    went additional steps through reference management and desktop
    publishing software. So there is some loss involved and the table
    does not contain an accurate image of what was published, although
    it is a good representative start.
    
    The references page (/report/references) shows the raw database
    source of each citation so you can see all the fields. It also
    shows a formatted version of the reference based on some rudimentary
    formatting code in this checkin.
    
    If we think the original reference table can be brought up to speed
    somehow with all the references that should be shown, then we can
    undertake the remainder of the effort to make the formatter 100%
    compliant with the IUCN standard + AESR extensions, e.g. personal
    communications and questionnaire replies.
    
    My guess is that the reference table could be cleaned, either on
    the web using some basic Rails CRUD scaffolding, or with a copy
    of the table in Excel or Access, given a day of dogged
    determination and a bottle of ibuprofen ... so I think this is
    the best way forward. I think the formatter, even though not 100%
    perfect, shows a lot of promise for something we created quickly.
    
    Marking Test and pushing onto staging for internal review and
    comment, but not ready to close this quite yet!
    
    Test #23

--- app/helpers/report_helper.rb
+++ app/helpers/report_helper.rb
@@ -23,4 +23,89 @@ module ReportHelper
     end
   end
 
+  # TODO stupid ampersand trick
+  def authify(authors)
+    formatted_authors = []
+    individual_authors = authors.split(';')
+    individual_authors.each do |name|
+      names = name.split(',')
+      if names.length>1
+        formatted_first_names = []
+        first_names = names[1].split(' ')
+        first_names.each do |first_name|
+          formatted_first_names << "#{first_name[0]}."
+        end
+        names[1] = formatted_first_names.join ' '
+      end
+      formatted_authors << names.join(', ')
+    end
+    result = formatted_authors.join(", ")
+    unless result =~ /\.$/
+      result = result + "."
+    end
+    result
+  end
+
+  def format_reference(ref)
+    formatted = []
+    formatted << authify(ref["Authors"])
+    formatted << " ("
+    formatted << ref["Year_pub"]
+    formatted << ")."
+    if ref["Ref_type"] == "Personal Communication"
+      formatted << " "
+      formatted << ref["Ref_type"]
+      formatted << ":"
+    end
+    formatted << " "
+    formatted << ref["Title"]
+    if ref["Type_work"] != "PDF document" and ref["Type_work"] != "Letter" and ref["Type_work"] != "Unpublished Report" and ref["Type_work"] != "Verbal information" and !ref["Type_work"].blank?
+      formatted << " ("
+      formatted << ref["Type_work"]
+      formatted << ")"
+    end
+    formatted << "."
+    unless ref["Place_pub"].blank?
+      formatted << " "
+      formatted << ref["Place_pub"]
+      formatted << ": "
+    end
+    unless ref["Publisher"]==ref["Authors"] or ref["Publisher"].blank?
+      formatted << " "
+      formatted << ref["Publisher"]
+      formatted << "."
+    end
+    url_shown = false
+    if ref["Type_work"] == "PDF document"
+      url_shown = true
+      formatted << " Retrieved "
+      formatted << ref["Number"] # TODO strftime
+      formatted << ", from "
+      formatted << link_to(ref["Url"], ref["Url"])
+      formatted << "."
+    end
+    if ref["Type_work"] == "Letter"
+      formatted << " Letter to "
+      formatted << authify(ref["Sec_authors"])
+      formatted << ", "
+      formatted << ref["Date_pub"] # TODO strftime
+      formatted << "."
+    end
+    if ref["Type_work"] == "Verbal information"
+      formatted << " Verbal information to "
+      formatted << authify(ref["Sec_authors"])
+      formatted << ", "
+      formatted << ref["Date_pub"] # TODO strftime
+      formatted << "."
+    end
+    unless url_shown
+      unless ref["Url"].blank?
+        formatted << " URL: "
+        formatted << link_to(ref["Url"], ref["Url"])
+        formatted << "."
+      end
+    end
+    formatted.join
+  end
+
 end
