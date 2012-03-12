c9de6474ddcc61d3ff77f6e4a261b663a2848971
Rob Heittman <heittman.rob@gmail.com>
2012-03-12 05:00:35 -0400

    Attempt to set content disposition to "attachment"
    Worth a try; attempts to set the content-disposition header on
    S3 file downloads so that they don't load directly in the browser
    but instead are downloaded as attachments. If this approach
    doesn't work, there is a more elaborate one involving redirects
    documented here:
    
    http://thewebfellas.com/blog/2009/8/29/protecting-your-paperclip-downloads
    
    Test #237

--- app/models/population_submission_attachment.rb
+++ app/models/population_submission_attachment.rb
@@ -14,6 +14,6 @@ class PopulationSubmissionAttachment < ActiveRecord::Base
       :access_key_id => ENV['S3_ACCESS_KEY_ID'],
       :secret_access_key => ENV['S3_SECRET_ACCESS_KEY']
     },
+    :s3_headers => { :content_disposition => 'attachment' },
     :path => "/:id/:filename"
-
 end
