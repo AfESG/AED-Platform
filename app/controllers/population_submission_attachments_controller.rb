b408c52bd3c45b47abd93cca5ff63b8330369733
Rob Heittman <heittman.rob@gmail.com>
2012-03-12 11:57:42 -0400

    Final (?) attempt at proper content-disposition
    Note: this appears to be working on a NEW attachment. An attachment
    you have already downloaded to view in the browser will continue
    to do that.
    
    Test #237

--- app/controllers/population_submission_attachments_controller.rb
+++ app/controllers/population_submission_attachments_controller.rb
@@ -10,7 +10,7 @@ class PopulationSubmissionAttachmentsController < ApplicationController
   def download
     head(:not_found) and return if (attachment = PopulationSubmissionAttachment.find_by_id(params[:id])).nil?
     path = attachment.file.path
-    uri = attachment.file.s3_object.url_for(:read, :secure => false, :expires_in => 10.seconds)
+    uri = attachment.file.s3_object.url_for(:read, :secure => false, :expires_in => 10.seconds, :response_content_disposition => 'attachment' )
     redirect_to uri.to_s
   end
 end
