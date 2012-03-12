f3d643ab57db7e72b29857b691e9ba7e02c78b4d
Rob Heittman <heittman.rob@gmail.com>
2012-03-11 21:25:08 -0400

    Database support for paper_trail
    See #34

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120308020028) do
+ActiveRecord::Schema.define(:version => 20120312011811) do
 
   create_table "countries", :force => true do |t|
     t.string   "iso_code"
@@ -414,5 +414,17 @@ ActiveRecord::Schema.define(:version => 20120308020028) do
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+
+  create_table "versions", :force => true do |t|
+    t.string   "item_type",      :null => false
+    t.integer  "item_id",        :null => false
+    t.string   "event",          :null => false
+    t.string   "whodunnit"
+    t.text     "object"
+    t.datetime "created_at"
+    t.text     "object_changes"
+  end
+
+  add_index "versions", ["item_type", "item_id"], :name => "index_versions_on_item_type_and_item_id"
 
 end
