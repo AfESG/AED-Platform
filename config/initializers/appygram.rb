19371eb988e3f794980859ea8515a4b36b8cc41f
Rob Heittman <heittman.rob@gmail.com>
2012-11-12 17:26:44 -0500

    Update appygram version

--- config/initializers/appygram.rb
+++ config/initializers/appygram.rb
@@ -1,3 +1,6 @@
+require 'appygram'
+require 'appygram-rails'
+
 if defined? Appygram
-  Appygram.configure '9fe8238e887839ac9ea8eaefe2d68f16435bb0b1'
+  Appygram.configure :api_key => '9fe8238e887839ac9ea8eaefe2d68f16435bb0b1'
 end
