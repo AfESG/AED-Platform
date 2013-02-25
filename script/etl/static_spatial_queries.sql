d98fb2d3cf32806d3fcaf159aee86168010fc226
Rob Heittman <heittman.rob@gmail.com>
2013-02-25 10:48:26 -0500

    Improved, faster, more self-consistent range stack
    Less unioning works faster. Most of the system is a dynamic view
    stack so changes are propagated if the base layers are adjusted.
    Some static queries still required for speed. Country level info
    is summed to higher levels for better internal consistency.

--- script/etl/static_spatial_queries.sql
+++ script/etl/static_spatial_queries.sql
@@ -15,117 +15,165 @@ update survey_individual_registrations s set survey_geometry_id=survey_geo from 
 update survey_others s set survey_geometry_id=survey_geo from (select substr(input_zone,2)::integer id, survey_geo from import_geometries where input_zone like 'O%' and survey_geo is not null) i where s.id=i.id;
 
 ###
+### now unused tables (replaced by views)
 ###
-###
-
-drop table continental_simplified_range;
-create table continental_simplified_range as select range, rangequali range_quality, ST_Union(geometry) range_geometry from range_geometries group by range, rangequali;
 
 drop table continental_oversimplified_range;
-create table continental_oversimplified_range as select ST_Union(range_geometry) range_geometry from continental_simplified_range where range=1;
-
-drop table continental_range_metrics;
-create table continental_range_metrics as select range, range_quality, SUM(ST_Area(range_geometry::geography,true))/1000000 area_sqkm from continental_simplified_range where range=1 group by range, range_quality;
-
 drop table regional_simplified_range;
-create table regional_simplified_range as select region, range, range_quality, ST_Intersection(ST_SetSRID(range_geometry,4326),ST_SetSRID(geom,4326)) range_geometry from continental_simplified_range, region;
-
-drop table regional_range_metrics;
-create table regional_range_metrics as select region, range, range_quality, SUM(ST_Area(range_geometry::geography,true))/1000000 area_sqkm from regional_simplified_range where range=1 group by region, range, range_quality;
-
+drop table continental_simplified_range;
 drop table country_simplified_range;
-create table country_simplified_range as select cntryname country, range, range_quality, ST_Intersection(ST_SetSRID(range_geometry,4326),ST_SetSRID(geom,4326)) range_geometry from continental_simplified_range, country;
+drop table regional_range_metrics;
+drop table continental_range_metrics;
+drop table survey_geometry_locator;
+drop table area_of_range_extant;
+drop table area_of_range_covered;
+drop table area_of_range_covered_subtotals;
+drop table area_of_range_covered_unassessed;
+drop table area_of_range_covered_totals;
+drop table regional_range_table;
+drop table regional_range_totals;
+drop table continental_range_table;
+drop table continental_range_totals;
+
+###
+### static geo queries
+###
+
+drop table country_range;
+create table country_range as select g.cntryname country, g.range, g.rangequali range_quality, ST_SetSRID(ST_Intersection(ST_SetSRID(geometry,4326),ST_SetSRID(geom,4326)),4326) range_geometry from range_geometries g, country c where ST_Intersects(ST_SetSRID(geometry,4326),ST_SetSRID(geom,4326));
 
 drop table country_range_metrics;
-create table country_range_metrics as select country, range, range_quality, SUM(ST_Area(range_geometry::geography,true))/1000000 area_sqkm from country_simplified_range where range=1 group by country, range, range_quality;
+create table country_range_metrics as select 'Africa'::text continent, region, country, range, range_quality, SUM(ST_Area(range_geometry::geography,true))/1000000 area_sqkm from country_range join country on cntryname=country where range=1 group by region, country, range, range_quality order by region, country, range, range_quality;
 
-drop table survey_geometry_locator;
-create table survey_geometry_locator as select site_name, analysis_name, analysis_year, region, category, country, input_zone_id, geom survey_geometry from estimate_locator, import_geometries where input_zone=input_zone_id and geom is not null;
+###
+### derived metrics
+###
+
+drop view regional_range_metrics;
+create or replace view regional_range_metrics as select continent, region, range, range_quality, SUM(area_sqkm) area_sqkm from country_range_metrics group by continent, region, range, range_quality;
+
+drop view continental_range_metrics;
+create or replace view continental_range_metrics as select continent, range, range_quality, SUM(area_sqkm) area_sqkm from regional_range_metrics group by continent, range, range_quality;
+
+drop view survey_geometry_locator;
+create or replace view survey_geometry_locator as select distinct site_name, analysis_name, analysis_year, region, category, reason_change, population_estimate, country, input_zone_id, survey_geometry from estimate_locator, import_geometries where input_zone=input_zone_id and geom is not null;
+
+###
+### survey range intersections: static, expensive
+###
+
+drop table survey_geometry_locator_buffered;
+create table survey_geometry_locator_buffered as select site_name, analysis_name, analysis_year, region, category, reason_change, population_estimate, country, input_zone_id, ST_Buffer(survey_geometry,0.0000001) survey_geometry from survey_geometry_locator;
 
 drop table survey_range_intersections;
-create table survey_range_intersections as select analysis_name, analysis_year, region, category, country, range_quality, ST_Intersection(ST_Union(ST_SetSRID(ST_Buffer(survey_geometry,0.0000001,'quad_segs=2, endcap=flat'),4326)),ST_SetSRID(range_geometry,4326)) from survey_geometry_locator, continental_simplified_range where range=1 group by analysis_name, analysis_year, region, category, country, range_quality, range_geometry;
+create table survey_range_intersections as select analysis_name, analysis_year, region, category, l.country, range_quality, ST_Intersection(survey_geometry,range_geometry) from survey_geometry_locator_buffered l join country_range c on ST_Intersects(survey_geometry,range_geometry) where range=1;
 
 drop table survey_range_intersection_metrics;
 create table survey_range_intersection_metrics as select analysis_name, analysis_year, region, range_quality, category, country, ST_Area(st_intersection::geography,true)/1000000 area_sqkm from survey_range_intersections;
 
-drop table area_of_range_extant;
-create table area_of_range_extant as
+###
+### range study queries
+###
+
+drop table review_range;
+create table review_range as select s.* from survey_geometry_locator s where analysis_name='2013_africa' and analysis_year='2012' and ((reason_change='NP' and population_estimate=0) or (reason_change='RS' and population_estimate=0) or (reason_change='NG' and population_estimate=0) or (reason_change='DA') or (reason_change='DD'));
+
+drop table add_range;
+create table add_range as select s.* from survey_geometry_locator s where analysis_name='2013_africa' and analysis_year='2012' and ((reason_change='NP' and population_estimate>0) or (reason_change='NG' and population_estimate>0) or (reason_change='NG' and population_estimate>0));
+
+###
+### Area of range tables
+###
+
+drop view area_of_range_extant;
+create or replace view area_of_range_extant as
 select
+  k.region,
   k.country,
   k.known,
   p.possible,
   (k.known+p.possible) total
 from
 (select
+  m.region,
   m.country,
   sum(area_sqkm) known
 from country_range_metrics m
 where range=1 and range_quality='Known'
-group by m.country) k
+group by m.region, m.country) k
 join
 (select
+  m.region,
   m.country,
   sum(area_sqkm) possible
 from country_range_metrics m
 where range=1 and range_quality='Possible'
-group by m.country) p
+group by m.region, m.country) p
 on k.country = p.country
-order by country;
+order by region, country;
 
-drop table area_of_range_covered;
-create table area_of_range_covered as
+drop view area_of_range_covered;
+create or replace view area_of_range_covered as
 select
+  k.region,
   k.country,
   k.surveytype,
   k.known,
   p.possible,
-  (k.known+p.possible) total
+  ((CASE WHEN k.known IS NULL THEN 0 ELSE k.known END)
+   +
+   (CASE WHEN p.possible IS NULL THEN 0 ELSE p.possible END)) total
 from
 (select
   m.country,
+  m.region,
   t.surveytype,
   sum(area_sqkm) known
 from survey_range_intersection_metrics m
 join surveytypes t on t.category = m.category
 where m.analysis_name='2013_africa' and m.analysis_year=2012 and range_quality='Known'
-group by m.country, t.surveytype) k
-join
+group by m.country, m.region, t.surveytype) k
+left join
 (select
   m.country,
+  m.region,
   t.surveytype,
   sum(area_sqkm) possible
 from survey_range_intersection_metrics m
 join surveytypes t on t.category = m.category
 where m.analysis_name='2013_africa' and m.analysis_year=2012 and range_quality='Possible'
-group by m.country, t.surveytype) p
+group by m.country, m.region, t.surveytype) p
 on k.country = p.country and k.surveytype = p.surveytype
-order by country, surveytype;
+order by region, country, surveytype;
 
-drop table area_of_range_covered_subtotals;
-create table area_of_range_covered_subtotals as
+drop view area_of_range_covered_subtotals;
+create or replace view area_of_range_covered_subtotals as
 select
+  region,
   country,
   sum(known) known,
   sum(possible) possible,
   sum(total) total
 from area_of_range_covered
-group by country
-order by country;
+group by region, country
+order by region, country;
 
-drop table area_of_range_covered_unassessed;
-create table area_of_range_covered_unassessed as
+drop view area_of_range_covered_unassessed;
+create or replace view area_of_range_covered_unassessed as
 select
+  x.region,
   x.country,
   x.known - n.known known,
   x.possible - n.possible possible,
   x.total - n.total total
 from area_of_range_extant x join
 area_of_range_covered_subtotals n on x.country = n.country
-order by x.country;
+order by x.region, x.country;
 
-drop table area_of_range_covered_totals;
-create table area_of_range_covered_totals as
+drop view area_of_range_covered_totals;
+create view area_of_range_covered_totals as
 select
+  region,
   country,
   sum(known) known,
   sum(possible) possible,
@@ -135,10 +183,78 @@ from (
   union
   select * from area_of_range_covered_unassessed
 ) t
-group by country
-order by country;
-drop table regional_range_table;
-create table regional_range_table as
+group by region, country
+order by region, country;
+
+drop view regional_area_of_range_covered;
+create or replace view regional_area_of_range_covered as
+select
+  region,
+  surveytype,
+  sum(known) known,
+  sum(possible) possible,
+  sum(total) total
+from
+area_of_range_covered
+group by region, surveytype
+order by region, surveytype;
+
+drop view regional_area_of_range_covered_unassessed;
+create or replace view regional_area_of_range_covered_unassessed as
+select
+  region,
+  sum(known) known,
+  sum(possible) possible,
+  sum(total) total
+from area_of_range_covered_unassessed
+group by region
+order by region;
+
+drop view regional_area_of_range_covered_totals;
+create or replace view regional_area_of_range_covered_totals as
+select
+  region,
+  sum(known) known,
+  sum(possible) possible,
+  sum(total) total
+from area_of_range_covered_totals
+group by region
+order by region;
+
+drop view continental_area_of_range_covered;
+create or replace view continental_area_of_range_covered as
+select
+  surveytype,
+  sum(known) known,
+  sum(possible) possible,
+  sum(total) total
+from
+area_of_range_covered
+group by surveytype
+order by surveytype;
+
+drop view continental_area_of_range_covered_unassessed;
+create or replace view continental_area_of_range_covered_unassessed as
+select
+  sum(known) known,
+  sum(possible) possible,
+  sum(total) total
+from area_of_range_covered_unassessed;
+
+drop view continental_area_of_range_covered_totals;
+create or replace view continental_area_of_range_covered_totals as
+select
+  sum(known) known,
+  sum(possible) possible,
+  sum(total) total
+from area_of_range_covered_totals;
+
+###
+### Regional and continental range tables
+###
+
+drop view regional_range_table;
+create or replace view regional_range_table as
 select
   r.region,
   m.country,
@@ -155,8 +271,8 @@ from
 group by r.region, m.country
 order by r.region, m.country;
 
-drop table regional_range_totals;
-create table regional_range_totals as
+drop view regional_range_totals;
+create or replace view regional_range_totals as
 select
   region,
   sum(range_area) range_area,
@@ -169,8 +285,8 @@ regional_range_table
 group by region, regional_range
 order by region;
 
-drop table continental_range_table;
-create table continental_range_table as
+drop view continental_range_table;
+create or replace view continental_range_table as
 select
   'Africa'::text continent,
   r.region,
@@ -187,8 +303,8 @@ from
 group by n.continent, r.region
 order by n.continent, r.region;
 
-drop table continental_range_totals;
-create table continental_range_totals as
+drop view continental_range_totals;
+create or replace view continental_range_totals as
 select
   continent,
   sum(range_area) range_area,
@@ -200,4 +316,3 @@ from
 continental_range_table
 group by continent
 order by continent;
-
