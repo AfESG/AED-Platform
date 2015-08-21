baae70d705ac849c4e08fad16c77b9d9bceb62e1
Rob Heittman <rob.heittman@solertium.com>
2015-08-21 08:33:21 -0400

    Demonstrate some searching the HTML/SQL way

--- app/controllers/submission_search_controller.rb
+++ app/controllers/submission_search_controller.rb
@@ -1,5 +1,22 @@
 class SubmissionSearchController < ApplicationController
+
+  # FIXME: this is repeated from report_controller
+  def execute(*array)
+    sql = ActiveRecord::Base.send(:sanitize_sql_array, array)
+    return ActiveRecord::Base.connection.execute(sql)
+  end
+
   def index
+    # TODO: make :country work with some joins
+    if params[:country] and !params[:country].blank?
+      @search_results = execute <<-SQL
+        SELECT *
+        FROM population_submissions
+        WHERE completion_year=#{params[:survey_year]}
+          AND released=#{params[:released]}
+        LIMIT 10
+      SQL
+    end
     render layout: 'bootstrapped'
   end
 end
