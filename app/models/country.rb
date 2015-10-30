719f4a0c3cfc3a102bafdb26b9acf2f302b2f2a4
Carl Scott <carl.scott@solertium.com>
2015-10-30 16:02:14 -0400

    Model cleanup for Rails Admin.
    Cleaned up some associations in the models to make things
    more friendly to rails admin.

--- app/models/country.rb
+++ app/models/country.rb
@@ -2,9 +2,10 @@ class Country < ActiveRecord::Base
   has_paper_trail
 
   # this model is not web-serviceable
-  attr_accessible
+  # attr_accessible
 
-  has_many :species, :through => :species_range_state_countries, :source => :species_range_state_country
+  has_many :species_range_state_countries
+  has_many :species, :through => :species_range_state_countries # , :source => :species_range_state_country
   has_many :mike_sites
   def to_s
     name
--- app/models/species_range_state_country.rb
+++ app/models/species_range_state_country.rb
@@ -2,7 +2,7 @@ class SpeciesRangeStateCountry < ActiveRecord::Base
   has_paper_trail
 
   # this model is not mass-assignable
-  attr_accessible
+  # attr_accessible
 
   belongs_to :species
   belongs_to :country
