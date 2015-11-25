0b6b0e212d1b695584a9475fb5a691c4f0cf9d22
Rob Heittman <rob.heittman@solertium.com>
2015-11-25 17:44:45 -0500

    Authorize mass assignment of changes

--- app/models/change.rb
+++ app/models/change.rb
@@ -1,2 +1,13 @@
 class Change < ActiveRecord::Base
+
+  attr_accessible(
+    :change,
+    :analysis_year,
+    :country,
+    :replacement_name,
+    :replaced_strata,
+    :new_strata,
+    :reason_change
+  )
+
 end
