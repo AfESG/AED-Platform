1166ce19c31225c755a6dcfd72d27417883941c9
Rob Heittman <rob.heittman@solertium.com>
2016-01-08 04:27:37 -0500

    Status flag on changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160108064142) do
+ActiveRecord::Schema.define(version: 20160108092640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20160108064142) do
     t.datetime "updated_at",                   null: false
     t.string   "country",          limit: 255
     t.integer  "analysis_id"
+    t.string   "status"
   end
 
   create_table "continent", primary_key: "gid", force: :cascade do |t|
