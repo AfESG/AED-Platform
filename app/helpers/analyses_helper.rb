ffe1fa818fdcb4174ce5ff2f243cb7e7b903236a
Rob Heittman <rob.heittman@solertium.com>
2016-01-10 04:00:07 -0500

    Hide numbering from table, show useful info

--- app/helpers/analyses_helper.rb
+++ app/helpers/analyses_helper.rb
@@ -1,2 +1,19 @@
 module AnalysesHelper
+
+  def describe_change_strata(strata, year)
+    return '-' if strata=='-' or strata==''
+    out = ''
+    strata.split(/,\s*/).each do |input_zone_id|
+      estimate = @estimates[input_zone_id+'@'+year.to_s]
+      if estimate
+        out << "<div>#{estimate['completion_year']} #{estimate['stratum_name']}</div>"
+        out << "<div style='font-size: x-small'>#{estimate['short_citation']}</div>"
+        out << "<div style='font-size: x-small'>#{estimate['estimate_type']} / cat #{estimate['category']}: est. #{estimate['population_estimate']}, #{estimate['stratum_area']} km²</div>"
+      else
+        out << input_zone_id+'@'+year.to_s+"?"
+      end
+    end
+    out
+  end
+
 end
