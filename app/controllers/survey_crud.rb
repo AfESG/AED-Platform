AfESG/AED-Platform

commit 0c77b58474a681cdfe36738a801345c2149b819f

Repair submission step
There was a regression around the submission page, the last step
in finalizing a survey. This page contained the abstract and
citation entry fields, as well as the toggles for "submitted" and
"released" for superusers. Getting this page to display again
makes things work largely as before, although we may want to
revisit the value of the submission step as currently implemented.

Test #394

app/controllers/survey_crud.rb
@@ -100,7 +100,7 @@ module SurveyCrud
     render template: level_form if level_form
   end
 
-  def edit
+  def edit(level_form = nil)
     set_title
     @level = level_class.find(params[:id])
     find_parents @level

app/controllers/population_submissions_controller.rb
@@ -51,7 +51,7 @@ class PopulationSubmissionsController < ApplicationController
   end
 
   def submit
-    edit
+    edit 'population_submissions/submit'
   end
 
   helper_method :sort_column, :sort_direction

app/views/population_submissions/submit.html.slim
@@ -7,15 +7,15 @@
       = render :partial => "submission_info", :locals => { :s => @submission }
   .col-sm-8
     .submit_instructions
-      | When you are done with your submission, complete the fields here to begin
-      | the data review process. If you are publishing this data under a Creative
-      | Commons license, the information you have entered will be released to the
-      | public as soon as an administrator initially reviews the submission -
-      | usually 1-2 business days.
+      ' When you are done with your submission, complete the fields here to begin
+      ' the data review process. If you are publishing this data under a Creative
+      ' Commons license, the information you have entered will be released to the
+      ' public as soon as an administrator initially reviews the submission -
+      ' usually 1-2 business days.
       p
-      | REMINDER: Have you attached your survey report and relevant geometries? If not,
-      | please add them via the "Attach a file" link, located at the bottom of the summary
-      | on the left, prior to completing this submission.
+      ' REMINDER: Have you attached your survey report and relevant geometries? If not,
+      ' please add them via the "Attach a file" link, located at the bottom of the summary
+      ' on the left, prior to completing this submission.
 
     = semantic_form_for @population_submission do |f|
       = render "shared/error_messages", :target => @population_submission
@@ -25,17 +25,17 @@
         = f.input :citation
         - if @population_submission.submitted?
           div
-            | This population survey is already submitted.
+            ' This population survey is already submitted.
         - else
           .submit_confirmation
-            | By ticking this box, I acknowledge that I have the rights to submit
-            | this information to the African Elephant Specialist Group and request
-            | review of this submission.
+            ' By ticking this box, I acknowledge that I have the rights to submit
+            ' this information to the African Elephant Specialist Group and request
+            ' review of this submission.
           = f.input :submitted
         - if current_user.admin?
           = f.input :short_citation
           .submit_confirmation
-            | Admin Use Only: ticking this box will make this submission visible
-            | on the site, pursuant to its data use restrictions.
+            ' Admin Use Only: ticking this box will make this submission visible
+            ' on the site, pursuant to its data use restrictions.
           =f.input :released
       = f.actions

commit 8c0ce4af12b36346c23d54a3c13a80df936f39dd

Fix regression of survey link edit pencils
This regressed in 0c77b58. Fix keeps behavior intended in 394
without breaking the stratum level pencils.

See #394
Test #411

app/controllers/survey_crud.rb
@@ -104,7 +104,7 @@ module SurveyCrud
     render template: level_form if level_form
   end
 
-  def edit(level_form = nil)
+  def edit(explicit_level_form = nil)
     set_title
     @level = level_class.find(params[:id])
     find_parents @level
@@ -118,7 +118,11 @@ module SurveyCrud
       redirect_to @level
       return
     end
-    render template: level_form if level_form
+    if explicit_level_form
+      render template: explicit_level_form
+    elsif level_form
+      render template: level_form
+    end
   end
 
   def create