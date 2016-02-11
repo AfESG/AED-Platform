aea52fa93425138fe9c060eb43943380665e5e8c
Rob Heittman <rob.heittman@solertium.com>
2016-02-11 09:15:53 -0500

    Generic API support for history interrogation
    Hitting /history/:item_type/:item_id returns a JSON object with
    the full diff history of that specific object, with the actor
    (paper_trail 'whodunnit' field) resolved to an email address.
    
    Call is reasonably efficient, but if it must be called for many
    or nested objects, it could be improved.

---
+++ app/controllers/versions_controller.rb
@@ -0,0 +1,25 @@
+class VersionsController < ApplicationController
+
+  def history
+    @item_type = params[:item_type].gsub(/[^\w_]/,'')
+
+    users = {}
+    User.all.each do |user|
+      users[user.id] = user.email
+    end
+
+    @item = eval(@item_type).find(params[:id])
+    result = []
+    @item.versions.each do |version|
+      changeset = version.changeset
+      changeset.delete 'created_at'
+      changeset.delete 'updated_at'
+      result << {
+        whodunnit: users[version.whodunnit.to_i],
+        changeset: changeset
+      }
+    end
+    render :json => result
+  end
+
+end
