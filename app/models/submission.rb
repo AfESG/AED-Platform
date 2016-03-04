e4c12fc10b7b7357f462f3cae2a19f358d8cbf8b
Rob Heittman <rob.heittman@solertium.com>
2016-03-04 00:09:39 -0500

    Regressions specifying species on new submission
    See #384

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -5,7 +5,6 @@ class Submission < ActiveRecord::Base
   # except user_id which will be set in the controller
   attr_protected :created_at, :updated_at, :user_id
 
-  validates_presence_of :species
   validates_presence_of :country
   validates_presence_of :data_type
 
