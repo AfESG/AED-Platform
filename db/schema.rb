3c479e181ca2d62a41b792cfe6eed07bd6daf990
Rob Heittman <rob.heittman@solertium.com>
2016-03-04 21:37:35 -0500

    Introduce attribution, initially populate it
    This attributes all geometries with their submission citation.
    
    Test #328

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160304023719) do
+ActiveRecord::Schema.define(version: 20160305022222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -870,8 +870,9 @@ ActiveRecord::Schema.define(version: 20160304023719) do
   end
 
   create_table "survey_geometries", force: :cascade do |t|
-    t.geometry "geometry", limit: {:srid=>0, :type=>"geometry"}
-    t.geometry "geom",     limit: {:srid=>0, :type=>"geometry"}
+    t.geometry "geometry",    limit: {:srid=>0, :type=>"geometry"}
+    t.geometry "geom",        limit: {:srid=>0, :type=>"geometry"}
+    t.string   "attribution"
   end
 
   add_index "survey_geometries", ["geom"], name: "si_survey_geom", using: :gist
