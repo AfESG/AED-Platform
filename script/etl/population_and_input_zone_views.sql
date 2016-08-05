91622e6c57756725911096ead988e5994edc4717
Robert Reis <RobertR@vulcan.com>
2016-08-05 16:51:17 -0700

    start filling in input zone data

--- script/etl/population_and_input_zone_views.sql
+++ script/etl/population_and_input_zone_views.sql
@@ -7,14 +7,36 @@ CREATE OR REPLACE VIEW input_zones AS
       FROM survey_geometries
   ), iz_data AS (
       SELECT
-        trim(e.replacement_name)      AS name,
-        e.population_submission_id    AS population_id,
-        -- TODO data
-        st_multi(st_collect(sg.geom)) AS geom
+        trim(e.replacement_name)         AS name,
+        e.analysis_year                  AS analysis_year,
+        e.analysis_name                  AS analysis_name,
+        e.population_submission_id       AS population_id,
+        e.reason_change                  AS cause_of_change,
+        e.estimate_type                  AS survey_type,
+        e.category                       AS survey_reliability,
+        e.completion_year                AS survey_year,
+        sum(e.population_estimate)       AS population_estimate,
+        e.population_confidence_interval AS percent_cl,
+        e.short_citation                 AS source,
+        NULL                             AS pfs,
+        NULL                             AS area,
+        NULL                             AS long,
+        NULL                             AS lat,
+        st_multi(st_collect(sg.geom))    AS geom
       FROM estimate_factors_analyses_categorized e
         JOIN estimate_factors ef ON (e.input_zone_id = ef.input_zone_id)
         JOIN survey_geom sg ON (ef.survey_geometry_id = sg.id)
-      GROUP BY (trim(e.replacement_name), e.population_submission_id)
+      GROUP BY (
+        trim(e.replacement_name),
+        e.population_submission_id,
+        e.reason_change,
+        e.estimate_type,
+        e.category,
+        e.completion_year,
+        e.population_confidence_interval,
+        e.short_citation,
+        e.analysis_year,
+        e.analysis_name)
   )
   SELECT
     row_number()
