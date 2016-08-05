8c926468a812e21b57ba391d8a68c48c679d4c2b
Robert Reis <RobertR@vulcan.com>
2016-08-05 15:29:59 -0700

    add geoms to input zone and popuations

--- script/etl/population_and_input_zone_views.sql
+++ script/etl/population_and_input_zone_views.sql
@@ -1,23 +1,20 @@
---- population view
-CREATE OR REPLACE VIEW populations AS
-  SELECT
-    ps.id,
-    ps.site_name AS name,
-    s.country_id
-  FROM population_submissions ps
-    JOIN submissions s ON (ps.submission_id = s.id);
-
-
 -- input zone view
 CREATE OR REPLACE VIEW input_zones AS
-  WITH iz_data AS (
-      SELECT DISTINCT ON (trim(e.replacement_name))
-        trim(e.replacement_name) AS name,
-        ps.id                    AS population_id
+  WITH survey_geom AS (
+      SELECT
+        id                     AS id,
+        ST_SetSRID(geom, 4326) AS geom
+      FROM survey_geometries
+  ), iz_data AS (
+      SELECT
+        trim(e.replacement_name)      AS name,
+        e.population_submission_id    AS population_id,
+        -- TODO data
+        st_multi(st_collect(sg.geom)) AS geom
       FROM estimate_factors_analyses_categorized e
-        JOIN population_submissions ps ON (e.population_submission_id = ps.id)
-      ORDER BY trim(e.replacement_name)
-
+        JOIN estimate_factors ef ON (e.input_zone_id = ef.input_zone_id)
+        JOIN survey_geom sg ON (ef.survey_geometry_id = sg.id)
+      GROUP BY (trim(e.replacement_name), e.population_submission_id)
   )
   SELECT
     row_number()
@@ -25,7 +22,6 @@ CREATE OR REPLACE VIEW input_zones AS
       ORDER BY name ASC) AS id,
     *
   FROM iz_data;
-
 
 -- input zone years
 CREATE OR REPLACE VIEW input_zones_years AS
@@ -41,3 +37,15 @@ CREATE OR REPLACE VIEW input_zones_years AS
   FROM input_zones iz
     JOIN year_data yd ON (iz.name = yd.name)
   ORDER BY iz.id, yd.analysis_year;
+
+--- population view
+CREATE OR REPLACE VIEW populations AS
+  SELECT
+    ps.id                         AS id,
+    ps.site_name                  AS name,
+    s.country_id                  AS country_id,
+    st_multi(st_collect(iz.geom)) AS geom
+  FROM population_submissions ps
+    JOIN submissions s ON (ps.submission_id = s.id)
+    JOIN input_zones iz ON (iz.population_id = ps.id)
+  GROUP BY (ps.id, ps.site_name, s.country_id)
