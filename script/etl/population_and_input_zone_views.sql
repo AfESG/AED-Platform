5aedca98d7e3a2c77fdab6cbd423258300b7ec21
Robert Reis <RobertR@vulcan.com>
2016-08-08 11:01:25 -0700

    finish input zone view

--- script/etl/population_and_input_zone_views.sql
+++ script/etl/population_and_input_zone_views.sql
@@ -5,27 +5,54 @@ CREATE OR REPLACE VIEW input_zones AS
         id                     AS id,
         ST_SetSRID(geom, 4326) AS geom
       FROM survey_geometries
+      GROUP BY id
   ), iz_data AS (
       SELECT
-        trim(e.replacement_name)                                                  AS name,
-        e.analysis_year                                                           AS analysis_year,
-        e.analysis_name                                                           AS analysis_name,
-        e.population_submission_id                                                AS population_id,
-        e.reason_change                                                           AS cause_of_change,
-        e.estimate_type                                                           AS survey_type,
-        e.category                                                                AS survey_reliability,
-        e.completion_year                                                         AS survey_year,
-        sum(e.population_estimate)                                                AS population_estimate,
-        e.population_confidence_interval                                          AS percent_cl,
-        e.population_upper_confidence_limit - e.population_lower_confidence_limit AS backup_percent_cl,
-        e.short_citation                                                          AS source,
-        NULL                                                                      AS pfs,
-        NULL                                                                      AS area,
-        NULL                                                                      AS long,
-        NULL                                                                      AS lat,
-        st_multi(st_collect(sg.geom))                                             AS geom
+        trim(e.replacement_name)                   AS name,
+        e.analysis_year                            AS analysis_year,
+        e.analysis_name                            AS analysis_name,
+        e.population_submission_id                 AS population_id,
+        e.reason_change                            AS cause_of_change,
+        e.estimate_type                            AS survey_type,
+        e.category                                 AS survey_reliability,
+        e.completion_year                          AS survey_year,
+        sum(e.population_estimate)                 AS population_estimate,
+        e.population_confidence_interval           AS percent_cl,
+        e.population_lower_confidence_limit        AS guess_min,
+        e.population_lower_confidence_limit        AS guess_max,
+        e.short_citation                           AS source,
+        round(
+            log((((ed.definite + ed.probable + 0.001) /
+                  (ed.definite + ed.probable + ed.possible + ed.speculative + 0.001)) + 1) /
+                (ela.area_sqkm / rrt.range_area))) AS pfs,
+        sum(el.stratum_area)                       AS area,
+        CASE WHEN ps.longitude < 0
+          THEN
+            to_char(abs(ps.longitude), '990D9') || 'W'
+        WHEN ps.longitude = 0
+          THEN
+            '0.0'
+        ELSE
+          to_char(abs(ps.longitude), '990D9') || 'E'
+        END                                        AS lon,
+        CASE WHEN ps.latitude < 0
+          THEN
+            to_char(abs(ps.latitude), '990D9') || 'S'
+        WHEN ps.latitude = 0
+          THEN
+            '0.0'
+        ELSE
+          to_char(abs(ps.latitude), '990D9') || 'N'
+        END                                        AS lat,
+        st_multi(st_collect(sg.geom))              AS geom
       FROM estimate_factors_analyses_categorized e
         JOIN estimate_factors ef ON (e.input_zone_id = ef.input_zone_id)
+        JOIN estimate_locator el ON (e.input_zone_id = el.input_zone_id)
+        JOIN estimate_dpps ed ON (e.input_zone_id = ed.input_zone_id)
+        JOIN estimate_locator_areas ela ON (e.input_zone_id = ela.input_zone_id)
+        JOIN population_submissions ps ON (el.population_submission_id = ps.id)
+        JOIN regional_range_table rrt ON (el.country = rrt.country AND
+                                          el.analysis_name = rrt.analysis_name AND el.analysis_year = rrt.analysis_year)
         JOIN survey_geom sg ON (ef.survey_geometry_id = sg.id)
       GROUP BY (
         trim(e.replacement_name),
@@ -39,7 +66,15 @@ CREATE OR REPLACE VIEW input_zones AS
         e.population_lower_confidence_limit,
         e.short_citation,
         e.analysis_year,
-        e.analysis_name)
+        e.analysis_name,
+        ed.definite,
+        ed.probable,
+        ed.possible,
+        ed.speculative,
+        ela.area_sqkm,
+        rrt.range_area,
+        ps.longitude,
+        ps.latitude)
   )
   SELECT
     row_number()
@@ -47,6 +82,7 @@ CREATE OR REPLACE VIEW input_zones AS
       ORDER BY name ASC) AS id,
     *
   FROM iz_data;
+
 
 -- input zone years
 CREATE OR REPLACE VIEW input_zones_years AS
@@ -62,6 +98,7 @@ CREATE OR REPLACE VIEW input_zones_years AS
   FROM input_zones iz
     JOIN year_data yd ON (iz.name = yd.name)
   ORDER BY iz.id, yd.analysis_year;
+
 
 --- population view
 CREATE OR REPLACE VIEW populations AS
