8715b7a60a394eb7c4ad8f4311faae5e32067ab8
Robert Reis <RobertR@vulcan.com>
2016-08-08 15:38:26 -0700

    fix input zone view

--- script/etl/population_and_input_zone_views.sql
+++ script/etl/population_and_input_zone_views.sql
@@ -1,87 +1,88 @@
 -- input zone view
 CREATE OR REPLACE VIEW input_zones AS
-  WITH survey_geom AS (
+  WITH aggregate_data AS (
       SELECT
-        id                     AS id,
-        ST_SetSRID(geom, 4326) AS geom
-      FROM survey_geometries
-      GROUP BY id
-  ), iz_data AS (
-      SELECT
-        trim(e.replacement_name)                   AS name,
-        e.analysis_year                            AS analysis_year,
-        e.analysis_name                            AS analysis_name,
-        e.population_submission_id                 AS population_id,
-        e.reason_change                            AS cause_of_change,
-        e.estimate_type                            AS survey_type,
-        e.category                                 AS survey_reliability,
-        e.completion_year                          AS survey_year,
-        sum(e.population_estimate)                 AS population_estimate,
-        e.population_confidence_interval           AS percent_cl,
-        e.population_lower_confidence_limit        AS guess_min,
-        e.population_lower_confidence_limit        AS guess_max,
-        e.short_citation                           AS source,
-        round(
-            log((((ed.definite + ed.probable + 0.001) /
-                  (ed.definite + ed.probable + ed.possible + ed.speculative + 0.001)) + 1) /
-                (ela.area_sqkm / rrt.range_area))) AS pfs,
-        sum(el.stratum_area)                       AS area,
-        CASE WHEN ps.longitude < 0
-          THEN
-            to_char(abs(ps.longitude), '990D9') || 'W'
-        WHEN ps.longitude = 0
-          THEN
-            '0.0'
-        ELSE
-          to_char(abs(ps.longitude), '990D9') || 'E'
-        END                                        AS lon,
-        CASE WHEN ps.latitude < 0
-          THEN
-            to_char(abs(ps.latitude), '990D9') || 'S'
-        WHEN ps.latitude = 0
-          THEN
-            '0.0'
-        ELSE
-          to_char(abs(ps.latitude), '990D9') || 'N'
-        END                                        AS lat,
-        st_multi(st_collect(sg.geom))              AS geom
-      FROM estimate_factors_analyses_categorized e
+        trim(e.replacement_name)                                              AS name,
+        e.analysis_year                                                       AS analysis_year,
+        e.analysis_name                                                       AS analysis_name,
+        sum(e.population_estimate)                                            AS estimate,
+        sum(e.stratum_area)                                                   AS area,
+        st_multi(st_collect(ST_SetSRID(sg.geom, 4326)))                       AS geom,
+        least(5, greatest(1,
+                          round(log((((sum(ed.definite) + sum(ed.probable) + 0.001) /
+                                      (sum(ed.definite) + sum(ed.probable) +
+                                       sum(ed.possible) + sum(ed.speculative) + 0.001))
+                                     + 1) /
+                                    (sum(ela.area_sqkm) / rrt.range_area))))) AS pfs
+      FROM
+        estimate_locator e
+        JOIN estimate_locator_areas ela ON (e.input_zone_id = ela.input_zone_id
+                                            AND e.analysis_name = ela.analysis_name
+                                            AND e.analysis_year = ela.analysis_year)
         JOIN estimate_factors ef ON (e.input_zone_id = ef.input_zone_id)
-        JOIN estimate_locator el ON (e.input_zone_id = el.input_zone_id)
-        JOIN estimate_dpps ed ON (e.input_zone_id = ed.input_zone_id)
-        JOIN estimate_locator_areas ela ON (e.input_zone_id = ela.input_zone_id)
-        JOIN population_submissions ps ON (el.population_submission_id = ps.id)
-        JOIN regional_range_table rrt ON (el.country = rrt.country AND
-                                          el.analysis_name = rrt.analysis_name AND el.analysis_year = rrt.analysis_year)
-        JOIN survey_geom sg ON (ef.survey_geometry_id = sg.id)
-      GROUP BY (
-        trim(e.replacement_name),
-        e.population_submission_id,
-        e.reason_change,
-        e.estimate_type,
-        e.category,
-        e.completion_year,
-        e.population_confidence_interval,
-        e.population_upper_confidence_limit,
-        e.population_lower_confidence_limit,
-        e.short_citation,
-        e.analysis_year,
-        e.analysis_name,
-        ed.definite,
-        ed.probable,
-        ed.possible,
-        ed.speculative,
-        ela.area_sqkm,
-        rrt.range_area,
-        ps.longitude,
-        ps.latitude)
+        JOIN survey_geometries sg ON (ef.survey_geometry_id = sg.id)
+        JOIN estimate_dpps ed ON (e.input_zone_id = ed.input_zone_id
+                                  AND e.analysis_name = ed.analysis_name
+                                  AND e.analysis_year = ed.analysis_year)
+        JOIN regional_range_table rrt ON (e.country = rrt.country
+                                          AND e.analysis_name = rrt.analysis_name
+                                          AND e.analysis_year = rrt.analysis_year)
+      GROUP BY (trim(e.replacement_name), e.analysis_year, e.analysis_name, rrt.range_area)
   )
   SELECT
-    row_number()
-    OVER (
-      ORDER BY name ASC) AS id,
-    *
-  FROM iz_data;
+    trim(e.replacement_name)   AS name,
+    e.analysis_year            AS analysis_year,
+    e.analysis_name            AS analysis_name,
+    e.population_submission_id AS population_id,
+    CASE WHEN e.reason_change = 'NC'
+      THEN
+        '-'
+    ELSE
+      reason_change
+    END                        AS cause_of_change,
+    e.estimate_type            AS survey_type,
+    e.category                 AS survey_reliability,
+    e.completion_year          AS survey_year,
+    ad.estimate                AS population_estimate,
+    CASE WHEN e.population_upper_confidence_limit IS NOT NULL
+      THEN
+        CASE WHEN e.estimate_type = 'O'
+          THEN
+            to_char(e.population_upper_confidence_limit - e.population_estimate, '999,999') || '*'
+        ELSE
+          to_char(e.population_upper_confidence_limit - e.population_estimate, '999,999')
+        END
+    WHEN e.population_confidence_interval IS NOT NULL
+      THEN
+        to_char(ROUND(e.population_confidence_interval), '999,999')
+    ELSE
+      NULL
+    END                        AS percent_cl,
+    e.short_citation           AS source,
+    ad.pfs                     AS pfs,
+    ad.area                    AS area,
+    CASE WHEN ps.longitude < 0
+      THEN
+        to_char(abs(ps.longitude), '990D9') || 'W'
+    WHEN ps.longitude = 0
+      THEN
+        '0.0'
+    ELSE
+      to_char(abs(ps.longitude), '990D9') || 'E'
+    END                        AS lon,
+    CASE WHEN ps.latitude < 0
+      THEN
+        to_char(abs(ps.latitude), '990D9') || 'S'
+    WHEN ps.latitude = 0
+      THEN
+        '0.0'
+    ELSE
+      to_char(abs(ps.latitude), '990D9') || 'N'
+    END                        AS lat,
+    ad.geom                    AS geom
+  FROM estimate_locator e
+    JOIN aggregate_data ad ON (trim(e.replacement_name) = ad.name)
+    JOIN population_submissions ps ON (e.population_submission_id = ps.id);
 
 
 -- input zone years
