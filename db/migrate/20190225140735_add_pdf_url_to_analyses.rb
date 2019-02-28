c6b84b1016740c5fb6b9c7d5e6227e46f2b1d44a
Patrick <pcstout@gmail.com>
2019-02-28 06:47:10 -0700

    Update 2016 PDF URL

--- db/migrate/20190225140735_add_pdf_url_to_analyses.rb
+++ db/migrate/20190225140735_add_pdf_url_to_analyses.rb
@@ -2,6 +2,6 @@ class AddPdfUrlToAnalyses < ActiveRecord::Migration
   def change
     add_column :analyses, :pdf_url, :string, null: true
 
-    Analysis.find_by(analysis_year: 2015)&.update_attribute(:pdf_url, 'https://portals.iucn.org/library/sites/library/files/documents/SSC-OP-060_A.pdf')
+    Analysis.find_by(analysis_year: 2015)&.update_attribute(:pdf_url, 'https://www.dropbox.com/s/dl/7a8w3kk6r9hzm0r/AfESG 20African 20Elephant 20Status 20Report 202016.pdf')
   end
 end
